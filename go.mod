9457da89fd35783efbd050329c0047c83cc64516
Ahmed Belhoula <ahmed.belhoula-extern@renault.com>
2022-07-01 15:23:59 +0200

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RIDCHA-DATA/golang-rest-api
+
+go 1.18
---
+++ internal/api/api.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"fmt"
+
+	"github.com/RIDCHA-DATA/golang-rest-api/internal/api/router"
+	"github.com/RIDCHA-DATA/golang-rest-api/internal/pkg/config"
+	"github.com/RIDCHA-DATA/golang-rest-api/internal/pkg/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setConfiguration(configPath string) {
+	config.Setup(configPath)
+	db.SetupDB()
+	gin.SetMode(config.GetConfig().Server.Mode)
+}
+
+func Run(configPath string) {
+	if configPath == "" {
+		configPath = "config.dev.yml"
+	}
+
+	setConfiguration(configPath)
+	conf := config.GetConfig()
+	web := router.Setup()
+	fmt.Println("Go API REST Running on port " + conf.Server.Port)
+	fmt.Println("==================>")
+	_ = web.Run(":" + conf.Server.Port)
+}
